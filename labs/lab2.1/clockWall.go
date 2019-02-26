2c0cdd541f58f4610ae9bcd861085104cddc3c24
JsphSalazar <jsphsalazar@gmail.com>
2019-02-26 09:45:20 -0600

    func main implemented

--- labs/lab2.1/clockWall.go
+++ labs/lab2.1/clockWall.go
@@ -25,3 +25,30 @@ func (c *clock) watch(w io.Writer, r io.Reader) {
 		log.Printf("Can not read from %s: %s")
 	}
 }
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stderr, "Usage: clockWall NAME=HOST")
+		os.Exit(1)
+	}
+	clocks := make([]*clock, 0)
+	for _, a := range os.Args[1:] {
+		fields := strings.Split(a, "=")
+		if len(fields) != 2 {
+			fmt.Fprintf(os.Stderr, "Bad arg: %s\n", a)
+			os.Exit(1)
+		}
+		clocks = append(clocks, &clock{fields[0], fields[1]})
+	}
+	for _, c := range clocks {
+		conn, err := net.Dial("TCP", c.host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+		go c.watch(os.Stdout, conn)
+	}
+	for {
+		time.Sleep(time.Minute)
+	}
+}
