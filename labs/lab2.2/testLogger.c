a948bd77f7fa7d358a1f1b88b7e13823155ba40b
JsphSalazar <jsphsalazar@gmail.com>
2019-03-05 17:44:05 -0600

    Now running but need improvements

---
+++ labs/lab2.2/testLogger.c
@@ -0,0 +1,16 @@
+#include "logger.h"
+
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
+
+int main()
+{
+	infof("This is information test\n");
+	warnf("This is a %s test\n", "warn");
+	errorf("This is an %s %s\n", "error", "test");
+	panicf("This is a panic test\n");
+	return 0;
+}
+
